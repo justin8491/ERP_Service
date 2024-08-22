9b7dfe2e13946500300d86462b6e0274288cdc87
justin8491 <justin77748292@gmail.com>
2024-08-22 17:38:59 +0900

    프로퍼티 세팅 및 타임리프 실행완료

--- src/main/java/com/erp/mes/controller/MemberController.java
+++ src/main/java/com/erp/mes/controller/MemberController.java
@@ -1,5 +1,15 @@
 package com.erp.mes.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 public class MemberController {
+
+    @GetMapping(value = "/home")
+    public String home(){
+
+        return "home";
+    }
 }
