cb7c4e80db268fca4b3e677fa6d5bf76ce15f33f
justin8491 <justin77748292@gmail.com>
2024-08-26 17:39:59 +0900

    구매 발주 임시 커밋

--- src/main/java/com/erp/mes/dao/PurchaseDAO.java
+++ src/main/java/com/erp/mes/dao/PurchaseDAO.java
@@ -1,17 +1,27 @@
 package com.erp.mes.dao;
 
 import com.erp.mes.dto.ItemDTO;
+import com.erp.mes.dto.OrderDTO;
 import com.erp.mes.dto.PlanDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface PurchaseDAO {
-    /**
-     *
-     * @param planDTO
-     * @return
-     */
-    List<PlanDTO> plan(PlanDTO planDTO);
+
+    @Select("SELECT * FROM plan")
+    List<PlanDTO> plan(PlanDTO planDTO); // 조달 계회 리스트
+    List<OrderDTO> order(OrderDTO orderDTO); // 구매 발주서 리스트
+
+    int orderForm(OrderDTO orderDTO); // 구매 발주서 수정
+
+    int inspectionForm(Map<String, Object> map);
+
+    int inspection(InspectionDTO inspectionDTO);
+
+
+
 }
---
+++ src/main/java/com/erp/mes/dto/InspectionDTO.java
@@ -0,0 +1,21 @@
+package com.erp.mes.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Slf4j
+public class InspectionDTO {
+    private int ins_id;
+    private Date date;
+
+    private int statis;
+}
