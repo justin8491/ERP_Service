15ad2af2fa388a6d545d126cc8e24c699bab24b1
justin8491 <justin77748292@gmail.com>
2024-08-26 15:58:48 +0900

    item 및 plan DTO 병합전

--- src/main/java/com/erp/mes/dao/PurchaseDAO.java
+++ src/main/java/com/erp/mes/dao/PurchaseDAO.java
@@ -2,7 +2,9 @@ package com.erp.mes.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface PurchaseDAO {
-    
+    List<PlanDTO> plan(ItemDTO itemDTO, PlanDTO planDTO);
 }
