2a7c251800f9def7f32817d4a19cd12f72f239f3
justin8491 <justin77748292@gmail.com>
2024-08-26 17:13:13 +0900

    ee

--- src/main/java/com/erp/mes/dao/PurchaseDAO.java
+++ src/main/java/com/erp/mes/dao/PurchaseDAO.java
@@ -1,10 +1,17 @@
 package com.erp.mes.dao;
 
+import com.erp.mes.dto.ItemDTO;
+import com.erp.mes.dto.PlanDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface PurchaseDAO {
-    List<PlanDTO> plan(ItemDTO itemDTO, PlanDTO planDTO);
+    /**
+     *
+     * @param planDTO
+     * @return
+     */
+    List<PlanDTO> plan(PlanDTO planDTO);
 }
