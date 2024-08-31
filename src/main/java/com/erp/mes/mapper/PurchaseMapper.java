d393d051131162048a3f9d7b9c763f7969a43359
justin8491 <justin77748292@gmail.com>
2024-08-31 20:53:19 +0900

    쿼리문 수정

--- src/main/java/com/erp/mes/mapper/PurchaseMapper.java
+++ src/main/java/com/erp/mes/mapper/PurchaseMapper.java
@@ -16,12 +16,14 @@ public interface PurchaseMapper {
 
     @Select("""
             SELECT * FROM plan
-                        
             """)
-    List<PlanDTO> plan(); // 조달 계회 리스트
+    List<PlanDTO> plan(); // 조달 계획 리스트
 
-    @Insert("INSERT INTO `order` VALUE(0,#{item_id},#{order_code},date,leadtime,status,value)")
-    int orderCreate(Map<String, Object> map);
+    @Insert("""
+            INSERT INTO `order`(sup_id, plan_id, order_code, `date`, status, value) 
+            VALUE(#{item_id},#{order_code},date,leadtime,status,value)
+            """)
+    int orderCreate(Map<String, Object> map); // 구매 발주서 발행
 
     @Select("""
             select i.name, p.qty, p.leadtime, s.name, value
@@ -32,19 +34,28 @@ public interface PurchaseMapper {
             p.leadtime BETWEEN #{startDate} AND #{endDate}
             AND s.name = #{supplier_name}
             """)
-    List<OrderDTO> order(); // 구매 발주서 리스트
+    List<OrderDTO> order(); // 구매 발주서 리스트 - 필수 값 -> 납기, 협력회사 이름
 
-    @Update("UPDATE `order` SET order_code = #{order_code}, date = #{date}, leadtime = #{leadtime}, status = #{status}, value = #{value} WHERE order_id = #{order_id}")
-    int orderForm(Map<String, Object> map); // 구매 발주서 수정
+    @Update("""
+            UPDATE `order` SET 
+            leadtime = #{leadtime}, status = #{status}, value = #{value} 
+            WHERE order_id = #{order_id}
+            """)
+    int orderForm(Map<String, Object> map); // 구매 발주서 수정 - 납기, 상태, 금액 부분 변경 - 이외 사항 발주 새로 발행
 
     @Select("SELECT * FROM inspection")
     int inspection(); // 검수 확인
 
-    @Insert("INSERT INTO inspection VALUES(#{ins_id}, NOW(), #{status}, #{notice})")
+    @Insert("""
+            INSERT INTO inspection(`date`, status, notice) VALUES(NOW(), #{status}, #{notice})
+            """)
     int inspectionForm(Map<String, Object> map); // 검수 생성
 
-    @Update("#{order_code}")
-    int inspectionUpdate(Map<String, Object> map);
+    @Update("""
+            UPDATE inspection SET
+            status = #{status}, notice = #{notice}
+            """)
+    int inspectionUpdate(Map<String, Object> map); // 검수 업데이트
 
 
 }
