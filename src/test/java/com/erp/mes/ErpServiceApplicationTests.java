9703b1748879adb1264815bf2c2fc0886546cb5d
yunzzangZ <wjddbsgnl100@gmail.com>
2024-09-02 15:27:10 +0900

    메인 프론트 완료

---
+++ src/test/java/com/erp/mes/ErpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.mes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
