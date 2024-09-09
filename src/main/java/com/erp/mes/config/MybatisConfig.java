bff7053030b27077053ccc9e44008d393b470c3e
justin8491 <justin77748292@gmail.com>
2024-09-09 14:26:50 +0900

    mybatisconfig

--- src/main/java/com/erp/mes/config/MybatisConfig.java
+++ src/main/java/com/erp/mes/config/MybatisConfig.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan(basePackages = "com.erp.mes")
 @MapperScan("com.erp.mes.mapper")
-public class MyBatisConfig {
+public class MybatisConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
