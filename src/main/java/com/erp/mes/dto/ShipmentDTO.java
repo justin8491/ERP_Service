891436e447b48cf1a35e86a3b07d1bc02545c873
pepperismeow <pepperismeow@gmail.com>
2024-09-12 19:56:58 +0900

    재고리포트 exel pdf 다운 추가

--- src/main/java/com/erp/mes/dto/ShipmentDTO.java
+++ src/main/java/com/erp/mes/dto/ShipmentDTO.java
@@ -15,8 +15,10 @@ public class ShipmentDTO {
     private int shipId;        // 출고 ID
     private int stkId;         // 재고 ID
     private LocalDate reqDate; // 요청 날짜
-    private int reqQty;        // 요청 수량
-    private int qty;           // 실제 출고 수량
+    private Integer reqQty;    // 요청 수량
+    private Integer qty;       // 실제 출고 수량
     private String status;     // 출고 상태
     private String loc;        // 출고 위치
+    private String itemName;   // 품목 이름 JOIN
+    private Integer availableQty; // 재고 수량 JOIN
 }
